2d0cfb62bfe86ee7aca722cb0711dbc3af1ec0b6
Artem <65169511+aranooke@users.noreply.github.com>
2022-09-13 07:23:05 +0300

    Animation hello
    It`s animation system

--- js/index.js
+++ js/index.js
@@ -1,4 +1,48 @@
-function animatePage()
-{
+class Animation {
+    constructor()
+    {
+        const blocks = [".skills-block",".flex-left",".flex-right"];
+        const fadeTime = 800;
+        this.blocks = blocks; 
+    }
+    hideBlock() // hide all elements
+    {
+        
+        this.blocks.forEach((value)=>{
+            $(value).hide();
+        })
+            /* $(".skills-block").hide();
+                $(".flex-left").hide();
+                $(".flex-right").hide(); /** */
+        
+    }
+    fadeAndAnimation()
+    {
+        this.blocks.forEach((value)=>{
+            $(value).fadeIn(this.fadeTime);
+        })
+    }
 
-}               
+
+
+}
+// ----------------------------------Main Part----------------------------------
+var anim = new Animation();
+const fadeTime = 800; // time to date objects
+function fadeAndAnimation()
+{
+    /*
+    $(".flex-left").fadeIn(fadeTime);
+    $(".flex-right").fadeIn(fadeTime);
+    $(".skills-block").fadeIn(fadeTime);
+    */
+}
+
+
+
+$(document).ready(function(){
+    anim.hideBlock();
+    anim.fadeAndAnimation();
+    
+    
+})
